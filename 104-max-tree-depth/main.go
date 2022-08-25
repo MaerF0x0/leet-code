a7e529c4e1285fc00b419a4614d49da7c6250c1d
Mike Graf <mikegraf000@gmail.com>
2022-08-25 14:12:08 -0500

    104-max-tree-depth/main.go

---
+++ 104-max-tree-depth/main.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+
+    if root == nil {
+        return 0
+    }
+    
+    l, r := maxDepth(root.Left), maxDepth(root.Right)
+    if l > r {
+        return 1 + l
+    } else {
+        return 1 + r
+    }
+    
+}
+
